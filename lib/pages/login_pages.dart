1f77f9f04020206632dbc9b812c53a9babb9ce14
tristanhanif <tristanhanif1769@gmail.com>
2026-04-16 13:17:50 +0700

    upgrade auth

--- lib/pages/login_pages.dart
+++ lib/pages/login_pages.dart
@@ -143,8 +143,6 @@ class _LoginPageState extends State<LoginPage> {
                   ),
 
                   const SizedBox(height: 16),
-
-/// Username
           _buildInputField(
             controller: _emailController,
             label: "Username",
--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../models/user.dart';
 
 class AuthProvider extends ChangeNotifier {
